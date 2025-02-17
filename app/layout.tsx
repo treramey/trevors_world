af127ea1d6e8ee782e121f36b41a12e0a268e133
Trevor Ramey <hello@trevors.email>
2025-02-17 00:25:44 -0500

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,136 +9,136 @@ import { baseUrl } from "./sitemap";
 import localFont from "next/font/local";
 
 const inter = Inter({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 const nbArchitect = localFont({
-	src: [
-		{
-			path: "../public/fonts/NBArchitektStd/NBArchitektStd-Bold.otf",
-			weight: "700",
-			style: "normal",
-		},
-		{
-			path: "../public/fonts/NBArchitektStd/NBArchitektStd-Regular.otf",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "../public/fonts/NBArchitektStd/NBArchitektStd-Light.otf",
-			weight: "400",
-			style: "normal",
-		},
-	],
-	variable: "--font-nb-architect",
+  src: [
+    {
+      path: "../public/fonts/NBArchitektStd/NBArchitektStd-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/NBArchitektStd/NBArchitektStd-Regular.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/NBArchitektStd/NBArchitektStd-Light.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-nb-architect",
 });
 
 const nbArchitectNeue = localFont({
-	src: [
-		{
-			path: "../public/fonts/NBArchitektStd/NBArchitektStdNeue-Bold.otf",
-			weight: "700",
-			style: "normal",
-		},
-		{
-			path: "../public/fonts/NBArchitektStd/NBArchitektStdNeue-Regular.otf",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "../public/fonts/NBArchitektStd/NBArchitektStdNeue-Light.otf",
-			weight: "400",
-			style: "normal",
-		},
-	],
-	variable: "--font-nb-architect-neue",
+  src: [
+    {
+      path: "../public/fonts/NBArchitektStd/NBArchitektStdNeue-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/NBArchitektStd/NBArchitektStdNeue-Regular.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/NBArchitektStd/NBArchitektStdNeue-Light.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-nb-architect-neue",
 });
 
 const mondwest = localFont({
-	src: [
-		{
-			path: "../public/fonts/Mondwest/PPMondwest-Regular.otf",
-			weight: "500",
-			style: "normal",
-		},
-	],
-	variable: "--font-mondwest",
+  src: [
+    {
+      path: "../public/fonts/Mondwest/PPMondwest-Regular.otf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-mondwest",
 });
 
 const neueBit = localFont({
-	src: [
-		{
-			path: "../public/fonts/Mondwest/PPNeueBit-Bold.otf",
-			weight: "500",
-			style: "normal",
-		},
-	],
-	variable: "--font-neuebit",
+  src: [
+    {
+      path: "../public/fonts/Mondwest/PPNeueBit-Bold.otf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-neuebit",
 });
 
 export const metadata: Metadata = {
-	metadataBase: new URL(baseUrl),
-	title: {
-		default: "Next.js Portfolio Starter",
-		template: "%s | Next.js Portfolio Starter",
-	},
-	description: "This is my portfolio.",
-	openGraph: {
-		title: "My Portfolio",
-		description: "This is my portfolio.",
-		url: baseUrl,
-		siteName: "My Portfolio",
-		locale: "en_US",
-		type: "website",
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: "Next.js Portfolio Starter",
+    template: "%s | Next.js Portfolio Starter",
+  },
+  description: "This is my portfolio.",
+  openGraph: {
+    title: "My Portfolio",
+    description: "This is my portfolio.",
+    url: baseUrl,
+    siteName: "My Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={cx(
-					inter.variable,
-					nbArchitect.variable,
-					nbArchitectNeue.variable,
-					neueBit.variable,
-					mondwest.variable,
-					"bg-primary-100 text-gray-950",
-				)}
-			>
-				<main className="flex relative flex-col min-h-screen antialiased tracking-wide cursor-default selection:bg-gray-600/30">
-					<div className="flex-1 flex-grow w-full">
-						<div className="flex-col p-4 mx-auto w-full max-w-screen-2xl min-h-screen">
-							{/* <Navbar /> */}
-							{children}
-							<Footer />
-						</div>
-					</div>
-					<Analytics />
-					<SpeedInsights />
-					<div className="fixed inset-0 z-50 w-full h-screen opacity-30 pointer-events-none mix-blend-overlay crt-line"></div>
-					<div className="absolute inset-x-0 bottom-0 z-0 w-full bg-repeat-x sm:opacity-60 h-[8rem] bg-[length:10px_240px] bg-[url('/bg-gradient.svg')] pointer-events-none"></div>
-				</main>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cx(
+          inter.variable,
+          nbArchitect.variable,
+          nbArchitectNeue.variable,
+          neueBit.variable,
+          mondwest.variable,
+          "bg-primary-100 text-gray-950",
+        )}
+      >
+        <main className="flex relative flex-col min-h-screen antialiased tracking-wide cursor-default selection:bg-gray-600/30">
+          <div className="flex-1 flex-grow w-full">
+            <div className="flex-col p-4 mx-auto w-full max-w-screen-2xl min-h-screen">
+              {/* <Navbar /> */}
+              {children}
+              <Footer />
+            </div>
+          </div>
+          <Analytics />
+          <SpeedInsights />
+          <div className="fixed inset-0 z-50 w-full h-screen opacity-30 pointer-events-none mix-blend-overlay crt-line"></div>
+          <div className="absolute inset-x-0 bottom-0 z-0 w-full bg-repeat-x sm:opacity-60 h-[8rem] bg-[length:10px_240px] bg-[url('/bg-gradient.svg')] pointer-events-none"></div>
+        </main>
+      </body>
+    </html>
+  );
 }
